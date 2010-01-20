c6e162adf78b8fcee9adb14e1c971dd68d9a911a
magicalhat <yuki@apps.magicalhat.jp>
2010-01-20 17:51:39 +0900

    Initial commit

---
+++ lib/DBIx/Skinny/Mixin/UpdateOrCreate.pm
@@ -0,0 +1,71 @@
+package DBIx::Skinny::Mixin::UpdateOrCreate;
+our $VERSION = '0.01';
+
+use strict;
+use warnings;
+
+use Carp ( );
+
+
+sub register_method
+{
+    return {
+        update_or_create => \&update_or_create,
+        in_storage       => sub { shift->{in_storage} },
+    };
+}
+
+sub update_or_create
+{
+    my $self = shift;
+    my ($table, $cond, $args) = @_;
+
+    $self->{in_storage} = 0;
+
+    my $itr = $self->search($table, $cond);
+    my $row;
+
+    if ($itr->count > 1) {
+        Carp::carp "Could not update; Query returned more than one row";
+    }
+    elsif ($itr->count == 1) {
+        $self->{in_storage} = 1;
+        $row   = $itr->first;
+        %$args = (%$cond, %$args);
+        $row->update($args);
+    }
+    else {
+        %$args = (%$cond, %$args);
+        $row   = $self->insert($table, $args);
+    }
+
+    return $row;
+}
+
+1;
+__END__
+
+=head1 NAME
+
+DBIx::Skinny::Mixin::UpdateOrCreate -
+
+=head1 SYNOPSIS
+
+  use DBIx::Skinny::Mixin::UpdateOrCreate;
+
+=head1 DESCRIPTION
+
+DBIx::Skinny::Mixin::UpdateOrCreate is
+
+=head1 AUTHOR
+
+hatyuki E<lt>hatyuki@apps.magicalhat.jpE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
---
+++ t/01_update_or_create.t
@@ -0,0 +1,9 @@
+use strict;
+use warnings;
+use lib './t';
+use Test::More;
+use Test::Exception;
+use Mock::Model;
+
+
+done_testing;
